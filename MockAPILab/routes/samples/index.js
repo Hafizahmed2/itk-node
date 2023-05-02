9c879bdfb73b58b36dbd80ff6d7646b09ef84416
Shujaa Marwat <shujaa.khan@devsinc.com>
2023-05-02 17:49:09 +0500

    Created a POST method that adds a new loan object with an array of borrowers

--- MockAPILab/routes/samples/index.js
+++ MockAPILab/routes/samples/index.js
@@ -7,6 +7,20 @@ router.get("/", (req, res) => {
   res.json(loans);
 });
 
+function isValidLoan(loan) {
+  return (
+    typeof loan === "object" &&
+    loan !== null &&
+    Object.keys(loan).every((key) => key in loanSchema) &&
+    loan.borrowers.every(
+      (borrower) =>
+        typeof borrower === "object" &&
+        borrower !== null &&
+        Object.keys(borrower).every((key) => key in loanSchema.borrowers[0])
+    )
+  );
+}
+
 router.get("/:loanId", (req, res) => {
   const loanId = parseInt(req.params.loanId);
   const loan = loans.find((l) => l.loanId === loanId);
@@ -16,5 +30,16 @@ router.get("/:loanId", (req, res) => {
     res.status(404).send("Loan not found");
   }
 });
+
+router.post("/", (req, res) => {
+  const loan = req.body;
+  if (isValidLoan(loan)) {
+    loans.push(loan);
+    res.status(201).send("Loan created");
+  } else {
+    res.status(400).send("Invalid loan data");
+  }
+});
+
 
 module.exports = router;
