3f8a526caeae2b16c209d382ade2d7a536d2c26c
Shujaa Marwat <shujaa.khan@devsinc.com>
2023-05-02 17:48:00 +0500

    Created a GET method that gets one loan object based on loanId

--- MockAPILab/routes/samples/index.js
+++ MockAPILab/routes/samples/index.js
@@ -6,3 +6,15 @@ const { loans, loanSchema } = require('./data.js');
 router.get("/", (req, res) => {
   res.json(loans);
 });
+
+router.get("/:loanId", (req, res) => {
+  const loanId = parseInt(req.params.loanId);
+  const loan = loans.find((l) => l.loanId === loanId);
+  if (loan) {
+    res.json(loan);
+  } else {
+    res.status(404).send("Loan not found");
+  }
+});
+
+module.exports = router;
