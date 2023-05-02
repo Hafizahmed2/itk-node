b17abbe335596abed739a87fb78f7a89acd13c4c
Shujaa Marwat <shujaa.khan@devsinc.com>
2023-05-02 17:56:31 +0500

    Created a DELETE method that deletes a borrower based on loanId and pairId

--- MockAPILab/routes/samples/index.js
+++ MockAPILab/routes/samples/index.js
@@ -58,4 +58,23 @@ router.patch("/:loanId/borrowers/:pairId", (req, res) => {
   }
 });
 
+router.delete("/:loanId/borrowers/:pairId", (req, res) => {
+  const loanId = parseInt(req.params.loanId);
+  const pairId = parseInt(req.params.pairId);
+  const loan = loans.find((l) => l.loanId === loanId);
+  if (loan) {
+    const borrowerIndex = loan.borrowers.findIndex((b) => b.pairId === pairId);
+    if (borrowerIndex !== -1) {
+      loan.borrowers.splice(borrowerIndex, 1);
+      res.status(200).send("Borrower deleted");
+    } else {
+      res.status(404).send("Borrower not found");
+    }
+  } else {
+    res.status(404).send("Loan not found");
+  }
+});
+
+
+
 module.exports = router;
