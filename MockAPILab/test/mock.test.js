8cc5b77f1a6c5b6d016fb035799adb9a6527d3b0
Shujaa Marwat <shujaa.khan@devsinc.com>
2023-05-02 22:13:25 +0500

    Added test for PUT and PATCH method

--- MockAPILab/test/mock.test.js
+++ MockAPILab/test/mock.test.js
@@ -31,3 +31,86 @@ describe('GET /:loanId', () => {
   });
 });
 
+describe('POST /', () => {
+  it('creates a new loan', async () => {
+    const newLoan = {
+      loanId: 3,
+      borrowers: [
+        {
+          pairId: 1,
+          firstName: 'Alice',
+          lastName: 'Smith',
+          phone: '555-5555',
+        },
+      ],
+    };
+
+    const response = await request(app)
+      .post('/loans')
+      .send(newLoan)
+      .set('Content-Type', 'application/json');
+
+    expect(response.status).to.equal(201);
+    expect(response.text).to.equal('Loan created');
+  });
+
+  it('returns error if loan data is invalid (missing data)', async () => {
+    const invalidLoan = {
+      loanId: 4,
+      borrowers: [
+        {
+          firstName: 'Bob',
+        },
+      ],
+    };
+
+    const response = await request(app)
+      .post('/loans')
+      .send(invalidLoan)
+      .set('Content-Type', 'application/json');
+
+    expect(response.status).to.equal(400);
+    expect(response.text).to.deep.equal('Invalid loan data');
+  });
+});
+
+describe('PATCH /:loanId/borrowers/:pairId', () => {
+  it('should update a borrower if matching loanId and pairId', async () => {
+    const loanId = 1;
+    const pairId = 1;
+    const response = await request(app)
+      .patch(`/loans/${loanId}/borrowers/${pairId}`)
+      .send({ firstName: 'Updated', lastName: 'Borrower' });
+
+    expect(response.status).to.equal(200);
+    expect(response.text).to.deep.equal('Borrower updated');
+
+    const updatedLoan = loans.find((loan) => loan.loanId === loanId);
+    const updatedBorrower = updatedLoan.borrowers.find((borrower) => borrower.pairId === pairId);
+
+    expect(updatedBorrower.firstName).to.deep.equal('Updated');
+    expect(updatedBorrower.lastName).to.deep.equal('Borrower');
+  });
+
+  it('should return error status code when loanId is not found', async () => {
+    const loanId = 10;
+    const pairId = 1;
+    const response = await request(app)
+      .patch(`/loans/${loanId}/borrowers/${pairId}`)
+      .send({ firstName: 'Updated', lastName: 'Borrower' });
+
+    expect(response.status).to.equal(404);
+    expect(response.text).to.deep.equal('Loan not found');
+  });
+
+  it('should return error status code when pairId is not found', async () => {
+    const loanId = 1;
+    const pairId = 10;
+    const response = await request(app)
+      .patch(`/loans/${loanId}/borrowers/${pairId}`)
+      .send({ firstName: 'Updated', lastName: 'Borrower' });
+
+    expect(response.status).to.equal(404);
+    expect(response.text).to.deep.equal('Borrower not found');
+  });
+});
